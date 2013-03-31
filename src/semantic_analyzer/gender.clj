ee42be61ad9ea7224fcb2c35e0d9196ca50df185
sergey <sergey@sergey-U36JC.(none)>
2013-03-31 20:10:04 +0600

    Texts are changed

--- src/semantic_analyzer/gender.clj
+++ src/semantic_analyzer/gender.clj
@@ -10,9 +10,20 @@
       "меня" "тебя" "его" "ее" "нас" "вас" "их"
       "мне" "тебе" "ему" "ей" "нам" "вам" "им"
       "мной" "тобой" "ею" "нами" "вами" "ими"
-      "нем" "ней"])
+      "нем" "ней"
+      "себя" "сам" "себе" "собой"
+      "кто" "кого" "кому" "кем"
+      "что" "чего" "чему" "чем"
+      "такой" "такому" "такого" "таким"
+      "тот" "того" "тому" "тем" "это"
+      "этот" "этого" "этому" "этим"
+      "такой" "такого" "такому" "таким"])
+
+(def NEGATIVES
+     ["не" "нет" "ни" "ничуть" "никак" "нельзя"])
 
 (def pronounsCount (ref 0))
+(def negativesCount (ref 0))
 
 (defn printStatistics []
   "Prints statistics about pronouns"
@@ -21,34 +32,55 @@
 	   "/"
 	   (stat/getWordsCount)
 	   " = "
-	   (/ @pronounsCount (stat/getWordsCount) 0.01) "%"))
+	   (/ @pronounsCount (stat/getWordsCount) 0.01) "%")
+  (println "Отрицательных слов :"
+	   @negativesCount
+	   "/"
+	   (stat/getWordsCount)
+	   " = "
+	   (/ @negativesCount (stat/getWordsCount) 0.01) "%"))
 
-(defn updateValue [number]
+(defn updatePronounsValue [number]
   "Updates number of pronouns"
   (dosync (ref-set pronounsCount number)))
 
+(defn updateNegativesValue [number]
+  "Updates number of negatives"
+  (dosync (ref-set negativesCount number)))
+
 (defn addPronouns [number]
   "Increases number of pronouns with a given number"
-  (updateValue (+ number @pronounsCount)))
+  (updatePronounsValue (+ number @pronounsCount)))
+
+(defn addNegatives [number]
+  "Increases number of negatives with a given number"
+  (updateNegativesValue (+ number @negativesCount)))
 
 (defn updateScore [words]
   "Updates data according to the given words input vector"
-  (let [toAdd (count
-	       (clojure.set/intersection (set words)
-					 (set PRONOUNS)))]
-    (addPronouns toAdd)))
+  (let [pronounsToAdd
+	(count (clojure.set/intersection (set words)
+					 (set PRONOUNS)))
+	negativesToAdd
+	(count (clojure.set/intersection (set words)
+					 (set NEGATIVES)))]
+    (addPronouns pronounsToAdd)
+    (addNegatives negativesToAdd)))
 
 (defn resetStatistics
   "Resets statistics"
   []
-  (updateValue 0))
+  (updatePronounsValue 0)
+  (updateNegativesValue 0))
 
-(def FEMALE_PRONOUN_CONSTANT 5.5)
+(def FEMALE_PRONOUN_CONSTANT 10)
 
 (defn getGender
   "Returns suggested text author's gender"
   []
-  (if (> (/ @pronounsCount (stat/getWordsCount) 0.01)
+  (if (> (/
+	  (+ @pronounsCount @negativesCount)
+	  (stat/getWordsCount) 0.01)
 	 FEMALE_PRONOUN_CONSTANT)
     (:f GENDERS)
     (:m GENDERS)))
